77b9bde766e7f53672902dc7c570164624d43e62
tvaler25 <129217422+tvaler25@users.noreply.github.com>
2025-02-04 21:20:06 -0500

    Update read_ultrasonic.ino

--- Code/read_ultrasonic/read_ultrasonic.ino
+++ Code/read_ultrasonic/read_ultrasonic.ino
@@ -1,108 +1,90 @@
-// Define the pins for the ultrasonic sensor
-const int trigPin = 9; // Trig pin of the ultrasonic sensor
-const int echoPin = 10; // Echo pin of the ultrasonic sensor
-
 //Define servo motor connections:
 #include<Servo.h> //include server library
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-Servo ser; //create servo object to control a servo
-int openVal = 110; //Open gripper value
-int closeVal = 10; //Close gripper value
-=======
-=======
->>>>>>> Stashed changes
-Servo ser1; //create servo object to control a servo
+
+// Define the pins for the ultrasonic sensor
+const int trigPinRight = 9; // Trig pin of the ultrasonic sensor
+const int echoPinRight = 10; // Echo pin of the ultrasonic sensor
+const int trigPinLeft  = 7;
+const int echoPinLeft  = 8;
+
+// Create Servo Objects
+Servo ser1; 
 Servo ser2;
-int openVal = 120; //Open gripper value
-int closeVal = 75; //Close gripper value
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
+
+const int servo_left_pin = 5;
+const int servo_right_pin = 11; 
+
+int upValRight = 120; //Right Paddle Up position
+int downValRight = 75; //right paddle down position
+int upValLeft = 40;  //left paddle up position
+int downValLeft = 170; //left paddle down position
+
 
 // Variables for the duration of the pulse and the distance
-long duration;
-long distance_mm;
+long duration_right;
+long distance_mm_right;
+
+long duration_left;
+long distance_mm_left; 
 
 
 void setup() {
-  // put your setup code here, to run once:
-  // Initialize serial communication
+
   Serial.begin(9600);
 
   // Define pin modes
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
+  pinMode(trigPinRight, OUTPUT);
+  pinMode(trigPinLeft, OUTPUT);
+  pinMode(echoPinRight, INPUT);
+  pinMode(echoPinLeft, INPUT);
 
-  //Sets the servo pin and opens gripper:
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-  ser.attach(5);// server is connected at pin 5
-  ser.write(closeVal);// the servo will move according to position 
-=======
-=======
->>>>>>> Stashed changes
-  ser1.attach(5);// server is connected at pin 5
+  ser1.attach(servo_right_pin);// servo is connected at pin 5
+
+  ser1.write(downValLeft);// the servo will move according to position 
   
+  ser2.attach(servo_right_pin);
+  ser2.write(downValRight);
 
-  ser1.write(closeVal);// the servo will move according to position 
-
-  ser2.attach(11);
-  ser2.write(closeVal);
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-  delay(2000);//delay for the servo to get to the position
+  delay(2000); //delay for the servo to get to the position
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 // Clear the trigPin by setting it LOW for 2 microseconds
-  digitalWrite(trigPin, LOW);
+  digitalWrite(trigPinRight, LOW);
   delayMicroseconds(2);
 
   // Set the trigPin HIGH for 10 microseconds to trigger the sensor
-  digitalWrite(trigPin, HIGH);
+  digitalWrite(trigPinRight, HIGH);
   delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
+  digitalWrite(trigPinRight, LOW);
 
   // Read the duration of the echo pulse from the echoPin
-  duration = pulseIn(echoPin, HIGH);
+  duration_right = pulseIn(echoPinRight, HIGH);
 
   // Calculate the distance in centimeters
-  distance_mm = duration * 0.34 / 2.0;
+  distance_mm_right = duration_right * 0.34 / 2.0;
 
   //open gripper
-  if(distance_mm<=35)
+  if(distance_mm_right<=35)
   {
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-    ser.write(openVal);// the servo will move according to position 
-    delay(3000);//delay for the servo to get to the position
-    ser.write(closeVal);// the servo will move according to position
-=======
-=======
->>>>>>> Stashed changes
-    ser1.write(openVal);// the servo will move according to position 
+
+
+    ser1.write(upValLeft);// the servo will move according to position 
     delay(1000);//delay for the servo to get to the position
-    ser2.write(openVal);
+    ser2.write(upValRight);
     delay(3000);
-    ser1.write(closeVal);// the servo will move according to position
+    ser1.write(downValLeft);// the servo will move according to position
     delay(1000);
-    ser2.write(closeVal);
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
+    ser2.write(downValRight);
+
   }
 
   
   // Print the distance to the serial monitor
   Serial.print("Distance: ");
-  Serial.println(distance_mm);
+  Serial.println(distance_mm_right);
   Serial.println(" mm");
   //Serial.print(duration);
 
