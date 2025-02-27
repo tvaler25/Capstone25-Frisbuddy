68369ca760e14d0dc1d065b3a23d8ad5af118caf
b-lazarus <129686114+b-lazarus@users.noreply.github.com>
2025-02-27 12:49:32 -0500

    net mount and raised track

--- Code/BensGreatestInventionCode/BensGreatestInventionCode.ino
+++ Code/BensGreatestInventionCode/BensGreatestInventionCode.ino
@@ -11,31 +11,53 @@ void setup() {
 
   servoGate.attach(servoGatePin);
   servoLinkage.attach(servoLinkagePin);
-  servoGate.write(0);
-  servoLinkage.write(0);
+  servoGate.write(70);
+  servoLinkage.write(10);
   
   Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-if (Serial.available()){
-  val = Serial.read();
+  if (Serial.available()){
+    val = Serial.read();
 
-if (val == 'h'){
- // for (int angle = 0; angle <= 70; angle += 1) {  // rotate slowly from 0 degrees to 120 degrees, one by one degree
-    // in steps of 1 degree
-    servoLinkage.write(70);  // control servo to go to position in variable 'angle'
-    delay(10);         // waits 10ms for the servo to reach the position
-  //}
-}
+    if (val == 'l') {
+    // for (int angle = 0; angle <= 70; angle += 1) {  // rotate slowly from 0 degrees to 120 degrees, one by one degree
+      servoLinkage.write(70);  // control servo to go to position in variable 'angle'
+      delay(10);         // waits 10ms for the servo to reach the position
+      
+    }
 
-if (val == 'l'){
- // for (int angle = 70; angle >= 0; angle -= 1) {  // rotate from 120 degrees to 0 degrees, one by one degree
-    servoLinkage.write(10);                        // control servo to go to position in variable 'angle'
-    delay(10);                               // waits 10ms for the servo to reach the position
+    if (val == '.') {
+    // for (int angle = 70; angle >= 0; angle -= 1) {  // rotate from 120 degrees to 0 degrees, one by one degree
+      servoLinkage.write(10);                        // control servo to go to position in variable 'angle'
+      delay(10);                               // waits 10ms for the servo to reach the position
+    }
+
+    if (val == 'g') {
+    // for (int angle = 0; angle <= 70; angle += 1) {  // rotate slowly from 0 degrees to 120 degrees, one by one degree
+      servoGate.write(70);  // control servo to go to position in variable 'angle'
+      delay(10);         // waits 10ms for the servo to reach the position
+      
+    }
+
+    if (val == 'b') {
+    // for (int angle = 70; angle >= 0; angle -= 1) {  // rotate from 120 degrees to 0 degrees, one by one degree
+      servoGate.write(10);                        // control servo to go to position in variable 'angle'
+      delay(10);                               // waits 10ms for the servo to reach the position
+    }
+
+    if (val == 'd') {
+      servoLinkage.write(70); //lift discs up
+      delay(1000);
+      servoGate.write(10); //open gate
+      delay(1500);
+      servoGate.write(70); //close gate
+      delay(1000);
+      servoLinkage.write(10);//lower linkage
+
+    }
   }
- // delay(10000);
+
 }
-}
-//}
