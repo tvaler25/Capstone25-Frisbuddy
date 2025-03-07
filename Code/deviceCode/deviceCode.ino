83433ef55d8aafa88c4e609ed989706fb63c803d
b-lazarus <129686114+b-lazarus@users.noreply.github.com>
2025-03-06 21:09:42 -0500

    devicecodeupdated

--- Code/deviceCode/deviceCode.ino
+++ Code/deviceCode/deviceCode.ino
@@ -1,14 +1,16 @@
 //Set digital pin numbers
 int motorDirPin = 2; int motorPWMPin = 3;
 int reorienterTriggerPin = 4; int reorienterEchoLeftPin = 5; int reorienterEchoRightPin = 6;
-int pusherServoPin = 7;
-int reserveGateServoPin = 8; int reserveLifterServoPin = 9;
-int aimerIn1Pin = 10; int aimerIn2Pin = 11;
-int IRReceiverSignal = 12;
+int reorienterServoRightPin = 7; int reorienterServoLeftPin = 8;
+int pusherServoPin = 9;
+int reserveGateServoPin = 10; int reserveLifterServoPin = 11;
+int aimerIn1Pin = 12; int aimerIn2Pin = 13;
+
 
 //set analog pin numbers
-int aimerLeftSignal = A0; int aimerRightSignal = A1;
+int aimerLeftSignalPin = A0; int aimerRightSignalPin = A1;
 int buzzerPin = A2;
+int IRReceiverPin = A3;
 
 //set constants for throughout program
 int motorPower = 165;
@@ -16,6 +18,15 @@ int motorPower = 165;
 void setup() {
   //Set pin modes
   pinMode(motorDirPin, OUTPUT); pinMode(motorPWMPin, OUTPUT);
+  pinMode(reorienterTriggerPin, OUTPUT); pinMode(reorienterEchoLeftPin, INPUT); pinMode(reorienterEchoRightPin, INPUT); 
+  pinMode(reorienterServoRightPin, OUTPUT); pinMode(reorienterServoRightPin, OUTPUT);
+  pinMode(pusherServoPin, OUTPUT);
+  pinMode(reserveGateServoPin, OUTPUT); pinMode(reserveLifterServoPin, OUTPUT);
+  pinMode(aimerIn1Pin, OUTPUT); pinMode(aimerIn2Pin, OUTPUT);
+
+  pinMode(aimerLeftSignalPin, INPUT); pinMode(aimerRightSignalPin, INPUT);
+  pinMode(buzzerPin, OUTPUT);
+  pinMode(IRReceiverPin, INPUT);
 
   //Write default values to pins
   analogWrite(motorDirPin, HIGH); digitalWrite(motorPWMPin, motorPower);
@@ -30,10 +41,27 @@ void loop() {
   do {}
   while(not seeting anything from reorienter and not seeing anything from remote control);
 
+  if(reorienter was seen) {
+    reorient
+  }
+
+  else if(remote control saw something) {
+    eject disc from reserve stack
+  }
 
   delay(3000); //wait for disc to reorient
 
+  
+  tone(buzzer, 750); //turn on buzzer
+
   //push disc into wheel
+  
+
+  delay(2000); //wait for disc to pass the aimer
+  noTone(buzzer); //turn off buzzer
+
+
+  //re-aim
 
 
 }
