debdfa9a30781692504f4b2d47eecd4cf9c1ca4f
JaeheeNa <jaeheena01@gmail.com>
2025-03-10 16:02:35 -0400

    Update Pusher_Code.ino

--- Code/Pusher/Pusher_Code/Pusher_Code.ino
+++ Code/Pusher/Pusher_Code/Pusher_Code.ino
@@ -2,7 +2,7 @@
 
 Servo pusherservo;
 
-const int pusherPin = 1;
+const int pusherPin = 7;
 
 void setup() {
   pinMode(pusherPin, OUTPUT);
@@ -11,18 +11,20 @@ void setup() {
 
 void loop() {
   // Move from 0 to 120 degrees slowly
-  for (int pos = 30; pos <= 150; pos += 1) { // Increase angle in small steps
+
+ 
+  for (int pos = 5; pos <= 90; pos += 1) { // Increase angle in small steps
     pusherservo.write(pos);
-    delay(20);  // Small delay for smoother motion
+    delay(40);  // Small delay for smoother motion
   }
   
   delay(1000); // Pause at 140 degrees
 
   // Move back from 140 to 0 degrees slowly
-  for (int pos = 150; pos >= 30; pos -= 1) { 
+  for (int pos = 90; pos >= 5; pos -= 1) { 
     pusherservo.write(pos);
-    delay(20);
+    delay(40);
   }
 
-  delay(1000); // Pause at 0 degrees
+  delay(1000); // Pause at 0 degrees 
 }
